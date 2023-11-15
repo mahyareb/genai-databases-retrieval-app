da7b6a423ce32407173618b6ad2ce9f310dfe6a8
Yuan Teoh <yuanteoh@google.com>
2023-11-15 15:58:04 -0800

    update sqlalchemy syntax

--- extension_service/datastore/providers/alloydb.py
+++ extension_service/datastore/providers/alloydb.py
@@ -13,19 +13,19 @@
 # limitations under the License.
 
 import asyncio
+from datetime import datetime
 from ipaddress import IPv4Address, IPv6Address
-from typing import Any, Dict, Literal, List, Tuple
+from typing import Any, Dict, Literal, Optional
 
 import asyncpg
-from pydantic import BaseModel
-from numpy import float32
-from google.cloud.alloydb.connector import Connector
 import sqlalchemy
-from sqlalchemy.ext.asyncio import create_async_engine
+from google.cloud.alloydb.connector import Connector
+from pgvector.asyncpg import register_vector
+from pydantic import BaseModel
 
 import models
-from .. import datastore
 
+from .. import datastore
 
 POSTGRES_IDENTIFIER = "alloydb-postgres"
 
@@ -42,11 +42,10 @@ class Config(BaseModel, datastore.AbstractConfig):
     database: str
 
 
-class Client(datastore.Client):
+class Client(datastore.Client[Config]):
     __pool: sqlalchemy.pool.Pool
 
-    @classmethod
-    @property
+    @datastore.classproperty
     def kind(cls):
         return "alloydb-postgres"
 
@@ -55,106 +54,394 @@ class Client(datastore.Client):
 
     @classmethod
     async def create(cls, config: Config) -> "Client":
-        def getconn(config):
+        def getconn():
             with Connector() as connector:
-                url = f"projects/{config.project}/locations/{config.region}/clusters/{config.cluster}/instances/{config.instance}"
                 conn = connector.connect(
-                    url,
-                    config.driver,
+                    f"projects/{config.project}/locations/{config.region}/clusters/{config.cluster}/instances/{config.instance}",
+                    "pg8000",
                     user=config.user,
                     password=config.password,
-                    db=config.database
+                    db=config.database,
                 )
             return conn
 
-        pool = create_async_engine(
-              "postgresql+asyncpg://",
-              creator=getconn(config),
+        pool = sqlalchemy.create_engine(
+            "postgresql+pg8000://",
+            creator=getconn,
         )
         if pool is None:
             raise TypeError("pool not instantiated")
         return cls(pool)
 
     async def initialize_data(
-        self, toys: List[models.Toy], embeddings: List[models.Embedding]
+        self,
+        airports: list[models.Airport],
+        amenities: list[models.Amenity],
+        flights: list[models.Flight],
     ) -> None:
-        async with self.__pool.connect() as conn:
+        with self.__pool.connect() as conn:
             # If the table already exists, drop it to avoid conflicts
-            await conn.execute("DROP TABLE IF EXISTS products CASCADE")
+            conn.execute(sqlalchemy.text("DROP TABLE IF EXISTS airports CASCADE"))
             # Create a new table
-            await conn.execute(
-                """
-                CREATE TABLE products(
-                  product_id VARCHAR(1024) PRIMARY KEY,
-                  product_name TEXT,
-                  description TEXT,
-                  list_price NUMERIC
+            conn.execute(
+                sqlalchemy.text(
+                    """
+                    CREATE TABLE airports(
+                      id INT PRIMARY KEY,
+                      iata TEXT,
+                      name TEXT,
+                      city TEXT,
+                      country TEXT
+                    )
+                    """
                 )
-                """
             )
             # Insert all the data
-            await conn.executemany(
-                """INSERT INTO products VALUES ($1, $2, $3, $4)""",
+            conn.executemany(
+                sqlalchemy.text(
+                    """INSERT INTO airports VALUES (:id, :iata, :name, :city, :country)"""
+                ),
                 [
-                    (t.product_id, t.product_name, t.description, t.list_price)
-                    for t in toys
+                    {
+                        "id": a.id,
+                        "iata": a.iata,
+                        "name": a.name,
+                        "city": a.city,
+                        "country": a.country,
+                    }
+                    for a in airports
                 ],
             )
 
-            await conn.execute("CREATE EXTENSION IF NOT EXISTS vector")
-            await conn.execute("DROP TABLE IF EXISTS product_embeddings")
-            await conn.execute(
-                """
-                CREATE TABLE product_embeddings(
-                    product_id VARCHAR(1024) NOT NULL REFERENCES products(product_id),
-                    content TEXT,
-                    embedding vector(768))
-                """
+            conn.execute(sqlalchemy.text("CREATE EXTENSION IF NOT EXISTS vector"))
+            # If the table already exists, drop it to avoid conflicts
+            conn.execute(sqlalchemy.text("DROP TABLE IF EXISTS amenities CASCADE"))
+            # Create a new table
+            conn.execute(
+                sqlalchemy.text(
+                    """
+                    CREATE TABLE amenities(
+                      id INT PRIMARY KEY,
+                      name TEXT,
+                      description TEXT,
+                      location TEXT,
+                      terminal TEXT,
+                      category TEXT,
+                      hour TEXT,
+                      content TEXT NOT NULL,
+                      embedding vector(768) NOT NULL
+                    )
+                    """
+                )
             )
             # Insert all the data
-            await conn.executemany(
-                """INSERT INTO product_embeddings VALUES ($1, $2, $3)""",
-                [(e.product_id, e.content, e.embedding) for e in embeddings],
+            conn.executemany(
+                sqlalchemy.text(
+                    """INSERT INTO amenities VALUES (:id, :name, :description, :location, :terminal, :category, :hour, :content, :embedding)"""
+                ),
+                [
+                    {
+                        "id": a.id,
+                        "name": a.name,
+                        "description": a.description,
+                        "location": a.location,
+                        "terminal": a.terminal,
+                        "category": a.category,
+                        "hour": a.hour,
+                        "content": a.content,
+                        "embedding": None
+                        if a.embedding is None
+                        else "[" + ",".join(str(e) for e in a.embedding) + "]",
+                    }
+                    for a in amenities
+                ],
             )
 
-    async def export_data(self) -> Tuple[List[models.Toy], List[models.Embedding]]:
-        toy_task = asyncio.create_task(self.__pool.fetch("""SELECT * FROM products"""))
-        emb_task = asyncio.create_task(
-            self.__pool.fetch("""SELECT * FROM product_embeddings""")
-        )
-
-        toys = [models.Toy.model_validate(dict(t)) for t in await toy_task]
-        embeddings = [models.Embedding.model_validate(dict(v)) for v in await emb_task]
-
-        return toys, embeddings
-
-    async def semantic_similiarity_search(
-        self, query_embedding: List[float32], similarity_theshold: float, top_k: int
-    ) -> List[Dict[str, Any]]:
-        results = await self.__pool.fetch(
-            """
-                WITH vector_matches AS (
-                    SELECT product_id, 1 - (embedding <=> $1) AS similarity
-                    FROM product_embeddings
-                    WHERE 1 - (embedding <=> $1) > $2
-                    ORDER BY similarity DESC
-                    LIMIT $3
+            # If the table already exists, drop it to avoid conflicts
+            conn.execute(sqlalchemy.text("DROP TABLE IF EXISTS flights CASCADE"))
+            # Create a new table
+            conn.execute(
+                sqlalchemy.text(
+                    """
+                    CREATE TABLE flights(
+                      id INTEGER PRIMARY KEY,
+                      airline TEXT,
+                      flight_number TEXT,
+                      departure_airport TEXT,
+                      arrival_airport TEXT,
+                      departure_time TIMESTAMP,
+                      arrival_time TIMESTAMP,
+                      departure_gate TEXT,
+                      arrival_gate TEXT
+                    )
+                    """
                 )
-                SELECT
-                    product_name,
-                    list_price,
-                    description
-                FROM products
-                WHERE product_id IN (SELECT product_id FROM vector_matches)
-            """,
-            query_embedding,
-            similarity_theshold,
-            top_k,
-            timeout=10,
+            )
+            # Insert all the data
+            conn.executemany(
+                sqlalchemy.text(
+                    """INSERT INTO flights VALUES (:id, :airline, :flight_number, :departure_airport, :arrival_airport, :departure_time, :arrival_time, :departure_gate, :arrival_gate)"""
+                ),
+                [
+                    {
+                        "id": f.id,
+                        "airline": f.airline,
+                        "flight_number": f.flight_number,
+                        "departure_airport": f.departure_airport,
+                        "arrival_airport": f.arrival_airport,
+                        "departure_time": f.departure_time,
+                        "arrival_time": f.arrival_time,
+                        "departure_gate": f.departure_gate,
+                        "arrival_gate": f.arrival_gate,
+                    }
+                    for f in flights
+                ],
+            )
+            conn.commit()
+
+    async def export_data(
+        self,
+    ) -> tuple[list[models.Airport], list[models.Amenity], list[models.Flight]]:
+        airport_task = asyncio.create_task(
+            self.__pool.fetch("""SELECT * FROM airports""")
+        )
+        amenity_task = asyncio.create_task(
+            self.__pool.fetch("""SELECT * FROM amenities""")
+        )
+        flights_task = asyncio.create_task(
+            self.__pool.fetch("""SELECT * FROM flights""")
         )
 
-        results = [dict(r) for r in results]
-        return results
+        airports = [models.Airport.model_validate(dict(a)) for a in await airport_task]
+        amenities = [models.Amenity.model_validate(dict(a)) for a in await amenity_task]
+        flights = [models.Flight.model_validate(dict(f)) for f in await flights_task]
+        return airports, amenities, flights
+
+    async def get_airport_by_id(self, id: int) -> Optional[models.Airport]:
+        with self.__pool.connect() as conn:
+            result = (
+                (
+                    conn.execute(
+                        sqlalchemy.text(
+                            """
+                      SELECT * FROM airports WHERE id=:id
+                    """
+                        ),
+                        {"id": id},
+                    )
+                )
+                .mappings()
+                .fetchone()
+            )
+
+        if result is None:
+            return None
+
+        res = models.Airport.model_validate(result)
+        return res
+
+    async def get_airport_by_iata(self, iata: str) -> Optional[models.Airport]:
+        with self.__pool.connect() as conn:
+            result = (
+                (
+                    conn.execute(
+                        sqlalchemy.text(
+                            """
+                      SELECT * FROM airports WHERE iata ILIKE :iata
+                    """
+                        ),
+                        {"iata": iata},
+                    )
+                )
+                .mappings()
+                .fetchone()
+            )
+
+        if result is None:
+            return None
+
+        res = models.Airport.model_validate(result)
+        return res
+
+    async def search_airports(
+        self,
+        country: Optional[str] = None,
+        city: Optional[str] = None,
+        name: Optional[str] = None,
+    ) -> list[models.Airport]:
+        with self.__pool.connect() as conn:
+            results = (
+                (
+                    conn.execute(
+                        sqlalchemy.text(
+                            """
+                    SELECT * FROM airports
+                    WHERE (CAST(:country AS TEXT) IS NULL OR country ILIKE :country)
+                    AND (CAST(:city AS TEXT) IS NULL OR city ILIKE :city)
+                    AND (CAST(:name AS TEXT) IS NULL OR name ILIKE '%' || :name || '%')
+                    """
+                        ),
+                        {
+                            "country": country,
+                            "city": city,
+                            "name": name,
+                        },
+                    )
+                )
+                .mappings()
+                .fetchall()
+            )
+
+        res = [models.Airport.model_validate(r) for r in results]
+        return res
+
+    async def get_amenity(self, id: int) -> Optional[models.Amenity]:
+        with self.__pool.connect() as conn:
+            result = (
+                (
+                    conn.execute(
+                        sqlalchemy.text(
+                            """
+                        SELECT id, name, description, location, terminal, category, hour
+                        FROM amenities WHERE id=:id
+                    """
+                        ),
+                        {"id": id},
+                    )
+                )
+                .mappings()
+                .fetchone()
+            )
+
+        if result is None:
+            return None
+
+        result = models.Amenity.model_validate(result)
+        return result
+
+    async def amenities_search(
+        self, query_embedding: list[float], similarity_threshold: float, top_k: int
+    ) -> list[models.Amenity]:
+        with self.__pool.connect() as conn:
+            results = (
+                (
+                    conn.execute(
+                        sqlalchemy.text(
+                            """
+                        SELECT id, name, description, location, terminal, category, hour
+                        FROM (
+                            SELECT id, name, description, location, terminal, category, hour, 1 - (embedding <=> :query_embedding AS similarity
+                            FROM amenities
+                            WHERE 1 - (embedding <=> :query_embedding) > :similarity_threshold
+                            ORDER BY similarity DESC
+                            LIMIT :top_k
+                        ) AS sorted_amenities
+                    """
+                        ),
+                        {
+                            "query_embedding": "["
+                            + ",".join(str(e) for e in query_embedding)
+                            + "]",
+                            "similarity_threshold": similarity_threshold,
+                            "top_k": top_k,
+                        },
+                    )
+                )
+                .mappings()
+                .fetchall()
+            )
+
+        res = [models.Amenity.model_validate(r) for r in results]
+        return res
+
+    async def get_flight(self, flight_id: int) -> Optional[models.Flight]:
+        with self.__pool.connect() as conn:
+            result = (
+                (
+                    conn.execute(
+                        sqlalchemy.text(
+                            """
+                        SELECT * FROM flights
+                        WHERE id = :flight_id
+                    """
+                        ),
+                        {"flight_id": flight_id},
+                    )
+                )
+                .mappings()
+                .fetchone()
+            )
+
+        if result is None:
+            return None
+
+        res = models.Flight.model_validate(result)
+        return res
+
+    async def search_flights_by_number(
+        self,
+        airline: str,
+        number: str,
+    ) -> list[models.Flight]:
+        with self.__pool.connect() as conn:
+            results = (
+                (
+                    conn.execute(
+                        sqlalchemy.text(
+                            """
+                        SELECT * FROM flights
+                        WHERE airline = :airline
+                        AND flight_number = :number
+                    """
+                        ),
+                        {
+                            "airline": airline,
+                            "number": number,
+                        },
+                    )
+                )
+                .mappings()
+                .fetchall()
+            )
+        res = [models.Flight.model_validate(r) for r in results]
+        return res
+
+    async def search_flights_by_airports(
+        self,
+        date: str,
+        departure_airport: Optional[str] = None,
+        arrival_airport: Optional[str] = None,
+    ) -> list[models.Flight]:
+        # Check if either parameter is null.
+        if departure_airport is None:
+            departure_airport = "%"
+        if arrival_airport is None:
+            arrival_airport = "%"
+        with self.__pool.connect() as conn:
+            results = (
+                (
+                    conn.execute(
+                        sqlalchemy.text(
+                            """
+                    SELECT * FROM flights
+                    WHERE departure_airport ILIKE :departure_airport
+                    AND arrival_airport ILIKE :arrival_airport
+                    AND departure_time > CAST(:datetime AS timestamp) - interval '1 day'
+                    AND departure_time < CAST(:datetime AS timestamp) + interval '1 day'
+                """
+                        ),
+                        {
+                            "departure_airport": departure_airport,
+                            "arrival_airport": arrival_airport,
+                            "datetime": datetime.strptime(date, "%Y-%m-%d"),
+                        },
+                    )
+                )
+                .mappings()
+                .fetchall()
+            )
+        res = [models.Flight.model_validate(r) for r in results]
+        return res
 
     async def close(self):
-        await self.__pool.close()
+        await self.__pool.dispose()
